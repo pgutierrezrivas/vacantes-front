d9b07f53855222b78deb9607fe4f59a7bcc20131
Patricia <pgutierrezrivas@gmx.es>
2025-05-06 17:06:45 +0200

    remove mocked data in perfil-usuario component

--- src/app/pages/usuario/perfil-usuario/perfil-usuario.component.ts
+++ src/app/pages/usuario/perfil-usuario/perfil-usuario.component.ts
@@ -31,32 +31,3 @@ export class PerfilUsuarioComponent implements OnInit, OnDestroy {
     throw new Error('Method not implemented.');
   }
 }
-
-/*versión datos mockeados
-import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../../../interfaces/usuario';
-import { AuthService } from '../../../security/auth.service';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'app-perfil-usuario',
-  imports: [CommonModule],
-  templateUrl: './perfil-usuario.component.html',
-  styleUrl: './perfil-usuario.component.css'
-})
-export class PerfilUsuarioComponent implements OnInit{
-
-usuario: Usuario | null = null;
-
-
-  constructor(private AuthService: AuthService) {}
-
-  ngOnInit(): void {
-    this.usuario = this.AuthService.getUsuario();
-  }
-
-  editarPerfil() {
-    throw new Error('Method not implemented.');
-    }
-
-}*/
